0678b45154e6b1eefafaea40c3b255d898dda9eb
satyabratapanda@net <2016satyabratapanda@gmil.com>
2020-03-28 13:28:12 +0530

    commit all

---
+++ Demo/src/main/java/com/cg/boot/SaleClass.java
@@ -0,0 +1,15 @@
+package com.cg.boot;
+
+public class SaleClass {
+
+	int arr[]= {12,3,4,5,6};
+	public void print()
+	{
+		int num=0;
+		for(int i=0;i<arr.length;i++)
+		{
+			num+=arr[i];
+			System.out.println(num);
+		}
+	}
+}
---
+++ Demo/src/main/java/com/cg/mode/MainClass.java
@@ -0,0 +1,11 @@
+package com.cg.mode;
+
+import com.cg.boot.SaleClass;
+
+public class MainClass {
+	public static void main(String args[])
+	{
+		SaleClass obj=new SaleClass();
+		obj.print();
+	}
+}
