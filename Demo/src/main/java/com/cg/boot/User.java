228a5a4926aace00549b710aa5d67b06401a8cf5
satyabratapanda@net <2016satyabratapanda@gmil.com>
2020-03-28 14:46:11 +0530

    user class in login feature branch

--- Demo/src/main/java/com/cg/boot/User.java
+++ Demo/src/main/java/com/cg/boot/User.java
@@ -1,5 +1,30 @@
 package com.cg.boot;
 
 public class User {
+	private String user_name;
+	private String password;
+	public User()
+	{
+		
+	}
+	
+	public User(String user_name, String password) {
+		super();
+		this.user_name = user_name;
+		this.password = password;
+	}
 
+	public String getUser_name() {
+		return user_name;
+	}
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
 }
