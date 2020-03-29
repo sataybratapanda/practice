152a3f595c1f864f2685bf503f78862433d77bcc
satyabratapanda@net <2016satyabratapanda@gmil.com>
2020-03-29 13:09:43 +0530

    enhanced for loop in salesclass

--- Demo/src/main/java/com/cg/boot/SaleClass.java
+++ Demo/src/main/java/com/cg/boot/SaleClass.java
@@ -5,10 +5,9 @@ public class SaleClass {
 	int arr[]= {12,3,4,5,6};
 	public void print()
 	{
-		int num=0;
-		for(int i=0;i<arr.length;i++)
+		
+		for(int num:arr)
 		{
-			num+=arr[i];
 			System.out.println(num);
 		}
 	}
